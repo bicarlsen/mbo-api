f53cd7f5495d3d9448612eb6afdb9f43de818bed
Brian Carlsen <carlsen.bri@gmail.com>
2018-07-26 07:44:21 +0200

    v2.x. Beginning of updates to accomodate MBO API v6.0 with breaking changes of authentication using API Key header.

---
+++ tests/mbo_Credentials.test.js
@@ -0,0 +1,83 @@
+var Credentials	= require( '../lib/classes/mbo_Credentials' ),
+	should 		= require( 'should' ),
+	log 		= require( 'fancy-log' );
+
+
+describe( 'MBO Credentials Unit Tests:', function() {
+	var creds;
+
+	describe( 'Constructor', function() {
+
+		describe( 'Source credenitals', function() {
+			var username,
+			password,
+			apiKey,
+			siteIds,
+			type;
+
+			before( function( done ) {
+				username 	= 'user';
+				password 	= 'pass';
+				apikey 		= 'key';
+				siteIds 	= -99;
+				type 		= 'source';
+
+				done();
+			} );
+
+			describe( 'Using SourceName and Password', function() {
+				before( function( done ) {
+					creds = new Credentials( 
+						{ 
+							username: username, 
+							password: password 
+						},
+
+						siteIds, 
+						type 
+					);
+
+					done();
+				} );
+
+				it( 'should have the proper credentials', function( done ) {
+					creds.credentials.sourceCredentials.should.eql( {
+						username: username, 
+						password: password 
+					} )
+
+					done();
+				} );
+
+				it( 'should have the correct authentication methods', function( done ) {
+					creds.authentication.should.eql( {
+						sourceCredentials: true,
+						apiKey: false
+					} );
+
+					done();
+				} );
+
+			} );
+		} ); // end Source Credentials
+
+		describe( 'User credentials', function() {
+			var username,
+				password,
+				type;
+
+			before( function( done ) {
+				username 	= 'user';
+				password 	= 'pass';
+				type 		= 'user';
+
+				done();
+			} );
+
+		} ); // end User Credentials
+
+		describe( 'Invalid credentials', function() {
+
+		} ); // end Invalid Credentials
+	} );
+} );
